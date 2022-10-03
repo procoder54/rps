740613ee6c0ba82b2f5e8c9d1e92c157ce9d3e14
Tarak Shaw <tarakspi11@gmail.com>
2022-10-03 12:00:55 +0530

    Created a function named 'playRound' that takes playerSelection and conputerSelection and returns winner

--- script.js
+++ script.js
@@ -1,7 +1,53 @@
-const choice=['rock','paper','scissor'];
-function getComputerChoice(){
-  let result = Math.floor(Math.random()*10)%3;
+const choice = ['rock', 'paper', 'scissor'];
+function getComputerChoice() {
+  let result = Math.floor(Math.random() * 10) % 3;
   return choice[result];
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return `It's a tie!`;
+  }
+
+  if (playerSelection === 'rock') {
+    if (computerSelection === 'paper') {
+      return `You Lose! Paper beats Rock`;
+    }
+    else {
+      return `You Won! Rock beats Scissor`
+    }
+  }
+  
+  if (playerSelection === 'paper') {
+    if (computerSelection === 'scissor') {
+      return `You Lose! Scissor beats Paper`;
+    }
+    else {
+      return `Your Won! Paper beats Rock`
+    }
+  }
+
+  if (playerSelection = 'scissor') {
+    if (computerSelection === 'rock') {
+      return `You Lose! Rock beats Scissor`;
+    }
+    else {
+      return `You Win! Scissor beats Paper`
+    }
+
+  }
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection))
+
+
+
+
+
+
+
+
+console.log(playerSelection.toLowerCase(), computerSelection);
