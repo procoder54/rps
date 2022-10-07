538cd479b9b9756f901a896d8c820660a939bc10
Tarak Shaw <tarakspi11@gmail.com>
2022-10-07 18:16:23 +0530

    Edited 'game' function so that when button is clicked it will play one round of game

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const choice = ['rock', 'paper', 'scissor'];
+const button = document.querySelectorAll(`.choice`);
+const score = document.querySelector(`#score`);
+let playerScore = 0;
+let computerScore = 0;
+
+
 function getComputerChoice() {
   let result = Math.floor(Math.random() * 10) % 3;
   return choice[result];
 }
+
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
@@ -32,33 +39,56 @@ function playRound(playerSelection, computerSelection) {
       return `You Lose! Rock beats Scissor`;
     }
     else {
-      return `You Win! Scissor beats Paper`
+      return `You Won! Scissor beats Paper`
     }
   }
 }
 
 
+function currentScore(result) {
+  if (result.textContent.substring(4, 7) === 'Won') {
+    playerScore += 1;
+  }
+  else if (result.textContent.substring(4, 8) === 'Lose') {
+    computerScore += 1;
+  }
+  const player = document.querySelector(`#player-score`);
+  const computer = document.querySelector(`#computer-score`);
+  player.textContent = `YOUR SCORE: ${playerScore}`;
+  computer.textContent = `COMPUTER SCORE: ${computerScore}`;
+  score.appendChild(player);
+  score.appendChild(computer);
+}
+
+
+function announceWinner() {
+  const winner = document.createElement(`div`)
+  if (computerScore === 5) {
+    winner.textContent = `>> YOU LOSE THE GAME`;
+  }
+  else if (playerScore === 5) {
+    winner.textContent = `>> YOU WON THE GAME`;
+  }
+  score.insertBefore(winner, score.firstChild)
+}
+
+
 function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-  while (playerScore !== 5 && computerScore !== 5) {
-    const playerSelection = 'Rock';
-    const computerSelection = getComputerChoice();
-    result = playRound(playerSelection.toLowerCase(), computerSelection);
-    if (result.slice(4, 5) === 'W') {
-      playerScore += 1;
-    }
-    if (result.slice(4, 5) === 'L') {
-      computerScore += 1;
-    }
-    console.log(playerScore, computerScore);
-    console.log(result)
-  }
-  if (playerScore === 5) {
-    console.log(`>> YOU WON THE GAME`)
-  }
-  else {
-    console.log(`>> YOU LOSE THE GAME`)
+  button.forEach(element => {
+    element.addEventListener(`click`, () => {
+      const playerSelection = element.textContent;
+      const computerSelection = getComputerChoice();
+      const leaderBoard = document.querySelector(`#result`);
+      const result = document.createElement(`p`);
+      result.textContent = playRound(playerSelection.toLowerCase(), computerSelection);
+      leaderBoard.insertBefore(result, leaderBoard.firstChild);
+      currentScore(result);
+      announceWinner();
+    })
+  });
+  if (playerScore === 5 || computerScore === 5) {
+    playerScore = 0;
+    computerScore = 0;
   }
 }
 
